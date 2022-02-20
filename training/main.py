01ac13ca185ca2aa82f82842acc55df960ce92f9
arnaudsm <arnaudsm66@gmail.com>
2022-02-19 19:11:54 -0800

    Refactor

--- training/main.py
+++ training/main.py
@@ -1,50 +1,49 @@
-import argparse
 from collections import Counter
 import sys
 
-def main(number, inputs):
-	answer = (0, [])
-	likes = Counter()
-	dislikes = Counter()
-	for like, dislike in inputs:
-		likes.update(like.split()[1:])
-		dislikes.update(dislike.split()[1:])
-	all_ingredients = set(likes.keys()) | set(dislikes.keys())
-	average = {
-		key: likes.get(key, 0) - dislikes.get(key, 0)
-		for key in all_ingredients
-	}
-	to_keep = []
-	for ingredient, hype in average.items():
-		if hype > 0:
-			to_keep.append(ingredient)
-	answer = (len(to_keep), to_keep)
-	return answer
+
+def load(filepath):
+    with open(filepath, "r") as file:
+        data = file.readlines()
+
+    return [
+        [
+            like.strip().split()[1:],
+            dislike.strip().split()[1:],
+        ] for like, dislike in zip(data[1::2], data[2::2])]
 
 
-def initialize(filepath):
-	with open(filepath, "r") as file:
-		data = file.readlines()
-	number = data[0]
-	inputs = []
-	for i in range(1, len(data)-1, 2):
-		inputs.append((data[i], data[i+1]))
-	return number, inputs
+def write_answer(answer):
+    buffer = str(len(answer)) + " "+" ".join(answer)
+    sys.stdout.buffer.write(buffer.encode())
 
 
-def format_answer(answer):
-	buffer = str(answer[0])
-	for ingredient in answer[1]:
-		buffer += " " + ingredient
-	return buffer
+def solve(inputs):
+    likes = Counter()
+    dislikes = Counter()
+    for like, dislike in inputs:
+        likes.update(like)
+        dislikes.update(dislike)
+
+    ingredients = set(likes) | set(dislikes)
+    average = {
+        key: likes.get(key, 0) - dislikes.get(key, 0)
+        for key in ingredients
+    }
+    to_keep = [ingredient for ingredient, hype in average.items() if hype > 0]
+    return to_keep
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("filepath", type=str)
-args = parser.parse_args()
-filepath = args.filepath
+def scoring(inputs, answer):
+    answer = set(answer)
 
-number, inputs = initialize(filepath)
-answer = main(number, inputs)
-buffer = format_answer(answer)
-sys.stdout.buffer.write(buffer.encode())
+    return sum(
+        set(like).issubset(answer) and set(dislike).isdisjoint(answer)
+        for like, dislike in inputs
+    )
+
+
+inputs = load("./a_an_example.in.txt")
+answer = solve(inputs)
+print("Answer", answer)
+print("Score", scoring(inputs, answer))
