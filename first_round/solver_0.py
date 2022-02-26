deb8c428453f4ce5832012e13b82d411f6f54b00
mifour <dufour.thomas@hotmail.fr>
2022-02-26 12:45:39 +0100

    start implementing assignment

--- first_round/solver_0.py
+++ first_round/solver_0.py
@@ -16,6 +16,44 @@ def solver():
         for skill, mate in assignments.items():
             people[mate][skill] += 1
 
+    def get_most_junior(candidates):
+        min_skill = float("inf")
+        most_junior = None
+        for candidate in candidates:
+            if candidate[1] < min_skill:
+                most_junior = candidate[0]
+                min_skill = candidate[1]
+        return most_junior
+
+    def assigning(assignments, people, roles):
+        # assign unique competences first
+        # then assign roles in desc order of requirements
+        # to the least experienced people that meet the requirements
+        # beware for mentoring
+        experienced = {}
+        for mate, skills in people.items():
+            for skill in skills:
+                experienced.setdefault(skill, []).append(mate)
+        unique_ressources = {skill: mates[0] for skill, mates in experienced if len(mates) == 1}
+        for role_name, requirement in roles.values():
+            if expert := unique_ressources.get(role_name):
+                assignments[role_name] = expert 
+        ordered_roles = sorted(
+            filter(lambda r: r["name"] not in assignments, roles),
+            key=lambda r: role["level"],
+            reverse=True
+        )
+        for role_name, requirement in ordered_roles:
+            candidates = [
+                (name, skills.get(role_name, 0)) 
+                for name, skills in people.items()
+                if name not in assignments and skills.get(role_name, 0) >= requirement -1
+            ]
+            # handle mentoring, mentor has to be in the assignments
+            candidate = get_most_junior(candidates) # ESN mentality >:(
+            # to continue
+
+
 
     print(workers)
     print(project)
@@ -33,9 +71,10 @@ def solver():
         project = project[1]
         roles = []
         assignments = {}
-        ... # magically assign roles to people
-        ... # proposed strategy: assign unique competence first, assign the least skilled people that meet the requirements (maybe mentored)
+        assigning(assignments, people, roles)
         update_people_skills(assignments, people)
         to_do.remove(project_name)
+        for role in project["roles"]:
+            roles.append(assignments[role["name"]])
         answer.append((project_name, roles))
     return answer
